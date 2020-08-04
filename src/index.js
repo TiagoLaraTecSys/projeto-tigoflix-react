d1a5797dbcdc3d35bd620cc0281daefdc4bcd929
tiago de lara ribeiro <laratecsys@gmail.com>
2020-08-03 22:57:34 -0300

    Adicionando rotas com react-router-dom

--- src/index.js
+++ src/index.js
@@ -3,11 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { BrowserRouter, Switch, Route} from 'react-router-dom'
 
+function CadastroVideo(){
+  return(
+    <div>
+      Cadastro de Video;
+    </div>
+  );
+}
+
+const Pagina404 = () => (
+  <div>
+    Página 404
+  </div>
+)
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={App} exact></Route>
+      <Route path="/cadastro/video" component={CadastroVideo}></Route>
+      <Route component={Pagina404}></Route>
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
