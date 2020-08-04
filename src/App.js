851b31f1f27aa52d180f222b8faef3c324365e38
tiago de lara ribeiro <laratecsys@gmail.com>
2020-08-03 22:34:32 -0300

    Adicionando o portfolio de videos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import Menu from './components/Menu'
+import MainImage from './components/MainImage'
+
+import BannerMain from './components/BannerMain'
+import Carousel from './components/Carousel'
+import Footer from './components/Footer'
+import dados_iniciais from './Data/dados_iniciais.json'
 
 function App() {
   return (
     <div >
       <Menu/>
+
+      <BannerMain
+        videoTitle={dados_iniciais.categorias[0].videos[0].titulo}
+        url={dados_iniciais.categorias[0].videos[0].url}
+        videoDescription={"O que é Front-end? Trabalhando na área" }
+      />
+
+      <Carousel
+
+        igoneFirstVideo
+        category={dados_iniciais.categorias[0]}
+      
+      />
+
     </div>
   );
 }
 
 export default App;
+ 
---
+++ src/components/MainImage/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './MainImage.css'
+function MainImage(){
+
+    return(
+        <section>
+        <div className="MainImage">
+            <div className="MainImageLeft">
+                <h1>5 Tipos de Pessoas que nunca vão enriquecer</h1>
+            </div>
+            <div className="MainImageRight">
+
+            </div>
+        </div>
+        </section>
+    );
+}
+
+export default MainImage;
